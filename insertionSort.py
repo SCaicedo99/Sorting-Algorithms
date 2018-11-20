5f441cdf9bac19bd2581e0f1fe36da021f5f63a0
Sebastian Caicedo <43104210+SCaicedo99@users.noreply.github.com>
2018-11-19 19:35:00 -0500

    Initial commit

---
+++ insertionSort.py
@@ -0,0 +1,15 @@
+# This sorting algorithm sorts the given array from highest to lowest, its running time is o(n^2),
+def insertionSort(alist):
+   for index in range(1,len(alist)):
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue:
+         position = position-1
+
+     alist[position]=currentvalue
+
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
